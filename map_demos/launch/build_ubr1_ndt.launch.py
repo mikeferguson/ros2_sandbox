e90c9ea82365ccb381702bae23f16b91b351549e
Michael Ferguson <mfergs7@gmail.com>
2023-12-07 15:40:31 -0500

    add map_demo work

---
+++ map_demos/launch/build_ubr1_ndt.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+# Build a map from the UBR-1 bagfile, using ndt_2d
+
+def generate_launch_description():
+
+    start_sync_slam_toolbox_node = Node(
+        parameters=[
+          get_package_share_directory("map_demos") + '/config/ndt_large_scale.yaml'
+        ],
+        package='ndt_2d',
+        executable='ndt_2d_map_node',
+        name='ndt_2d_node',
+        remappings=[('scan', 'base_scan')],
+        output='screen')
+
+    ld = LaunchDescription()
+    ld.add_action(start_sync_slam_toolbox_node)
+
+    return ld
---
+++ map_demos/launch/build_ubr1_slam_toolbox.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+# Build a map from the UBR-1 bagfile, using slam_toolbox
+
+def generate_launch_description():
+
+    start_sync_slam_toolbox_node = Node(
+        parameters=[
+          get_package_share_directory("map_demos") + '/config/slam_toolbox_online_sync.yaml'
+        ],
+        package='slam_toolbox',
+        executable='sync_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen')
+
+    ld = LaunchDescription()
+    ld.add_action(start_sync_slam_toolbox_node)
+
+    return ld
