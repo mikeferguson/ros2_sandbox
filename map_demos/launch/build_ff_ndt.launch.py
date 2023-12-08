f518cd4b2625b6548ddac9881e10e96f7cd3fe58
Michael Ferguson <mfergs7@gmail.com>
2023-12-08 09:01:01 -0500

    linting

--- map_demos/launch/build_ff_ndt.launch.py
+++ map_demos/launch/build_ff_ndt.launch.py
@@ -1,14 +1,14 @@
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+
 
 # Build a map from the small fire fighter bagfile, using ndt_2d
-
 def generate_launch_description():
 
     start_sync_slam_toolbox_node = Node(
         parameters=[
-          get_package_share_directory("map_demos") + '/config/ndt_small_scale.yaml'
+          get_package_share_directory('map_demos') + '/config/ndt_small_scale.yaml'
         ],
         package='ndt_2d',
         executable='ndt_2d_map_node',
--- map_demos/launch/build_ubr1_slam_toolbox.launch.py
+++ map_demos/launch/build_ubr1_slam_toolbox.launch.py
@@ -1,14 +1,14 @@
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+
 
 # Build a map from the UBR-1 bagfile, using slam_toolbox
-
 def generate_launch_description():
 
     start_sync_slam_toolbox_node = Node(
         parameters=[
-          get_package_share_directory("map_demos") + '/config/slam_toolbox_online_sync.yaml'
+          get_package_share_directory('map_demos') + '/config/slam_toolbox_online_sync.yaml'
         ],
         package='slam_toolbox',
         executable='sync_slam_toolbox_node',
